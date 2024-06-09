32c2c428b8ea53f9f092550dd3ff404f99cc42ff
Agus-Natalio <122061647+Agus-Natalio@users.noreply.github.com>
2024-06-09 19:55:45 -0300

    Delete Guspaion/TP5/ejercicio1 directory

--- Guspaion/TP5/ejercicio1/Edge.java
+++
@@ -1,7 +0,0 @@
-package tp5.ejercicio1;
-
-public interface Edge<T> {
-	public Vertex<T> getTarget();//Retorna el vertice destino de la arista
-	
-	public int getWeight();//Retorna el peso de la arista
-}
