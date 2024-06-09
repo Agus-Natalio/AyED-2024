4704934dbc29a1ea69310a76a6b5b2f5cb94ea44
Agus-Natalio <122061647+Agus-Natalio@users.noreply.github.com>
2024-06-09 19:55:38 -0300

    Delete Guspaion/tp5 directory

--- Guspaion/tp5/ejercicio1/Vertex.java
+++
@@ -1,9 +0,0 @@
-package tp5.ejercicio1;
-
-public interface Vertex<T> {
-	public T getData();//Retorna el dato del vertice
-	
-	public void setData(T data);//Reemplaza el dato del vertice
-	
-	public int getPosition();//Retorna la posicion del vertice en el grafo
-}
