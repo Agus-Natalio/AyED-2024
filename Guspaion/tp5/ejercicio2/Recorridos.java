6ca9e06f10e93a7857952235b9036cfd59a3a0c6
Agus-Natalio <122061647+Agus-Natalio@users.noreply.github.com>
2024-06-10 12:59:37 -0300

    Update Recorridos.java

--- Guspaion/tp5/ejercicio2/Recorridos.java
+++ Guspaion/tp5/ejercicio2/Recorridos.java
@@ -33,23 +33,23 @@ public class Recorridos<T> {
 	}
 	
 	public List<T> bfs(Graph<T> grafo){
-		boolean[] visited = new boolean[grafo.getSize()];
-		Queue<Vertex<T>> Q = new Queue<Vertex<T>>();
-		List<T> L = new LinkedList<T>();
-		Q.enqueue(grafo.getVertex(0));
-		while(!Q.isEmpty()) {
-			Vertex<T> aux = Q.dequeue();			
-			visited[aux.getPosition()] = true;
-			L.add(aux.getData());
-            for (Edge<T> edge : grafo.getEdges(aux)) {
-                Vertex<T> target = edge.getTarget();
-                int pos = target.getPosition();
-                if (!visited[pos]) {
-                    Q.enqueue(target);
-                    visited[pos] = true;
+	    boolean[] visited = new boolean[grafo.getSize()];
+            Queue<Vertex<T>> Q = new Queue<Vertex<T>>();
+	    List<T> L = new LinkedList<T>();
+	    Q.enqueue(grafo.getVertex(0));
+	    while(!Q.isEmpty()) {
+		Vertex<T> aux = Q.dequeue();			
+		visited[aux.getPosition()] = true;
+		L.add(aux.getData());
+            	for (Edge<T> edge : grafo.getEdges(aux)) {
+                    Vertex<T> target = edge.getTarget();
+                    int pos = target.getPosition();
+                    if (!visited[pos]) {
+                        Q.enqueue(target);
+                        visited[pos] = true;
+                    }
                 }
-            }
-		}
-		return L;
+	    }
+	    return L;
 	}
 }
